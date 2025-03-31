819b3b38092ce1310dd38c5feb44f9e49c53848d
wktr <94459237+WiktorProj@users.noreply.github.com>
2025-03-31 20:48:41 +0200

    modified:   About/install_dir.txt modified:   WorldNodes/Nodes/block_community_debug.lua new file:   WorldNodes/Nodes/block_simple_grow.lua

--- WorldNodes/Nodes/block_community_debug.lua
+++ WorldNodes/Nodes/block_community_debug.lua
@@ -4,4 +4,11 @@ function p.__get_tex() return "block_default" end
 function p.__main()
     set_default_block("XAR_SOLID_BORING")
     create_rect("XAR_MOSSY_AIR_UP_DOWN_TOP", 1,1,1, 14,14,14)
+    set_pos(14,14,14, "XAR_SOLID_BORING_EXPANDER")
+    set_pos(13,14,14, "simple_grow")
+    set_pos(14,13,14, "simple_grow")
+    add_bent(14,14,13, "bent_base_ring_green")
+    add_bent_s(13,13,7, "bent_base_waypoint", "Community Xar debug room! :D")
+    add_bent(13,13,9, "bent_base_ring_blue")
+    add_bent_s(13,13,11, "bent_base_txt", "This blue ring will take you to Mylantis!")
 end
---
+++ WorldNodes/Nodes/block_simple_grow.lua
@@ -0,0 +1,8 @@
+function p.__get_is_solid() return false end
+
+function p.__get_tex() return "" end
+
+function p.__main()
+    set_default_block("XAR_EMPTY_BORING")
+    add_bent(7,7,7, "bent_base_ring_red")
+end
