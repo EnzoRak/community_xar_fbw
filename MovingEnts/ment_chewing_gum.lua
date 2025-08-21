a02f9f23469499b5549c48b173e04df62440b752
Distort-Cubic <47984830+Distort-Cubic@users.noreply.github.com>
2025-08-20 22:08:25 -0400

    Fix for the fix for chewing gum, and continuing to debug the ritual block (WHY IS IT NOT WORKING!?!?)

--- MovingEnts/ment_chewing_gum.lua
+++ MovingEnts/ment_chewing_gum.lua
@@ -23,12 +23,13 @@ function p.__type_init(id)
     local aura_radius = 8.0
     ia_ment_new_static_var_f(id, "damage_aura_duration", 1.0)
     ia_ment_new_var_f(id, "damage_aura_next_time", 0.0, 60.0)
+    ia_ment_new_static_var_f(id, "damage_aura_radius", aura_radius)
     --[[
     ia_ment_new_static_var_b(id, "damage_aura_has", true)
     ia_ment_new_static_var_i(id, "damage_aura_damage", 0)
-    ia_ment_new_static_var_f(id, "damage_aura_radius", aura_radius)
-    ia_ment_new_static_var_b(id, "damage_aura_only_same_level", true)
     ]]
+
+    ia_ment_new_static_var_b(id, "damage_aura_only_same_level", true)
     ia_ment_set_builtin_var_f(id, "__gas_cloud_period", 0.05)
     ia_ment_set_builtin_var_f(id, "__gas_cloud_radius", aura_radius)
     ia_ment_set_builtin_var_f(id, "__gas_cloud_trigger_dist", 64.0)
@@ -81,12 +82,20 @@ function p.__on_update(inst_id, elapsed_time, elapsed_level_time)
     end
 
     if( the_time > aura_next_time - 0.1 ) then
-        --Setting the next aura time.
-        local next_aura_time = the_time + aura_length
-        ga_ment_set_f(inst_id, "damage_aura_next_time", next_aura_time)
-        --Adding an alarm.
-        ga_ment_set_alarm(inst_id, next_aura_time, "damage_aura_next")
+        local aura_radius = ga_ment_get_f(inst_id, "damage_aura_radius")
+        local start_level = ga_ment_get_i(inst_id, "__start_level")
+        if( aura_radius > 0 ) then
+            --Doing a radius check.
+            local sllp = ga_ment_get_sllp(inst_id)
+            local viewer_lp = ga_get_viewer_lp(start_level)
+            local dist_to_viewer = std.dist(sllp, viewer_lp)
+            if( dist_to_viewer > aura_radius ) then return end
+        end
 
+        if( ga_ment_b_exists_and_true(inst_id, "damage_aura_only_same_level") ) then
+            player_level = ga_get_viewer_level()
+            if( start_level ~= player_level ) then return end
+        end
         
         local damage = 0 + ga_get_i("xar.player.health.amount") + ga_get_i("xar.player.armor.amount") + ga_get_i("xar.player.shield.amount")
         --damage aura damage is curved
@@ -102,6 +111,13 @@ function p.__on_update(inst_id, elapsed_time, elapsed_level_time)
         local neg_dir = std.vec_scale(dir, -1.0)
         --it needs to be raw.
         game_top2.damage_player_from_dir(true, damage, neg_dir)
+
+        --Setting the next aura time.
+        local next_aura_time = the_time + aura_length
+        ga_ment_set_f(inst_id, "damage_aura_next_time", next_aura_time)
+        --Adding an alarm.
+        ga_ment_set_alarm(inst_id, next_aura_time, "damage_aura_next")
+
         --ga_console_print("Chewing Gum Damage Aura: " .. tostring(damage) .. " damage to player.")
     end
 end
--- WorldNodes/Nodes/block_sunflower_ritual.lua
+++ WorldNodes/Nodes/block_sunflower_ritual.lua
@@ -32,6 +32,8 @@ function p.__on_use(level, pos)
     leveldecrementer = leveldecrementer + 1
     ga_console_print("Sun found?  " .. tostring(sunfound))
     ga_print("Sun found?  " .. tostring(sunfound))
+    ga_console_print("Sun found? (Post) " .. leveldecrementer)
+    ga_print("Sun found?  (Post)" .. leveldecrementer)
     ga_ment_start(leveldecrementer, ga_chunk_id_and_lbp_to_bp(ga_get_viewer_ancestor_chunk_id(leveldecrementer), std.vec(7,7,7)), "ment_sun")
     ga_ment_end()
     ga_block_set_i(level, pos, "canuse", 0)
